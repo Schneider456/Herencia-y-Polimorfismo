36709c31cbdaaeec42ea57964cca936605dfa15f
Schneider456 <efrain_acdc@hotmail.com>
2017-03-02 19:38:04 -0600

    = new Update();

---
+++ EjercicioPromedios/CuentaBancaria.java
@@ -0,0 +1,11 @@
+public class CuentaBancaria {
+    private int saldo;
+    
+    public CuentaBancaria (int s) {
+        saldo = s;
+    }
+    
+    public int dimeSaldo () {
+        return saldo;
+    }
+}
