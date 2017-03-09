0a18367acd044a0b3b36a95c20cafa50f33f50d9
Schneider456 <efrain_acdc@hotmail.com>
2017-03-09 12:00:16 -0600

    = new Update(Pelota);

---
+++ EjercicioPromedios/CuentaBancaria.java
@@ -0,0 +1,11 @@
+public class CuentaBancaria {
+    private int saldo;
+    
+    public CuentaBancaria (int sald) {
+        saldo = sald;
+    }
+    
+    public int dimeSaldo () {
+        return saldo;
+    }
+}
